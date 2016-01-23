bc18101c98749106f1ec84ea837b3c05f00d20f7
Helge Waastad <helge.waastad@datametrix.no>
2016-01-23 12:57:17 +0100

    test build

--- src/attributes/buttion.js
+++ src/attributes/buttion.js
@@ -1,4 +1,32 @@
+import {inject, customAttribute,bindable} from 'aurelia-framework';
+import 'jquery-ui';
+import 'primeui';
 
+@customAttribute('p-button')
+@inject(Element)
 export class ButtonDirective {
-  
+  @bindable icon: String;
+  @bindable iconPos: String;
+  @bindable disabled: Boolean;
+
+  initialized: Boolean;
+
+  constructor(element){
+    this.element=element;
+    this.initialized=false;
+  }
+
+  bind(){
+    $(this.element).puibutton({
+      icon: this.icon,
+      iconPos:this.iconPos,
+    });
+    this.initialized=true;
+  }
+
+  detached() {
+    $(this.element).puibutton('destroy');
+    this.initialized=false;
+  }
+
 }
